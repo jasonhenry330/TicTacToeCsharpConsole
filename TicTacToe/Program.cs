b82c6b2ed5f6ca63a952e86fe6b63b1a5301a158
Jason H <jasonhenry330@gmail.com>
2020-04-06 20:24:49 -0400

    Initial commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using TicTacToe.Services;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tttService = new TicTacToeService();
+
+            do
+            {
+                tttService.InitializeBoard();
+                var playerTurn = BoardWinner.x;
+                var message = String.Empty;
+
+                Console.Clear();
+                Console.WriteLine("Would you like to play a game? How about a nice game of global thermal nuclear tic tac toe?");
+
+                while (!tttService.GameIsOver())
+                {
+                    if (!String.IsNullOrEmpty(message)) Console.WriteLine(message);
+                    Console.WriteLine();
+                    Console.WriteLine(tttService.GetBoardString());
+                    Console.Write(String.Format("Player {0} what cell would you like to occupy? ", playerTurn == BoardWinner.x ? "X" : "O"));
+
+                    var line = Console.ReadLine();
+                    //validate the entry 
+                    var digitRegex = new Regex(@"^\d$");
+                    if (digitRegex.IsMatch(line))
+                    {
+                        //process the entry
+                        if (tttService.UpdateCell(Convert.ToInt32(line), playerTurn))
+                        { 
+                            //update the player turn
+                            playerTurn = playerTurn == BoardWinner.x ? BoardWinner.o : BoardWinner.x;
+                            message = String.Empty;
+                        }
+                        else
+                        {
+                            message = "The selected cell is occupied.";
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid selection. Please choose a cell 1 through 9.";
+                    }
+
+                    Console.Clear();
+                }
+
+                var winner = tttService.GetGameWinner();
+
+                Console.Clear();
+                Console.WriteLine();
+                Console.WriteLine(tttService.GetBoardString());
+
+                switch (winner)
+                {
+                    case BoardWinner.x:
+                        Console.Write("X ");
+                        break;
+                    case BoardWinner.o:
+                        Console.Write("O ");
+                        break;
+                    case BoardWinner.cat:
+                    case BoardWinner.none:
+                        Console.Write("Cat ");
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.Write("wins! Play again? (y/n) ");
+            } while (Console.ReadLine() == "y");
+        }
+    }
+}
---
+++ TicTacToe/Services/TicTacToeService.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Text;
+
+namespace TicTacToe.Services
+{
+    public enum BoardWinner { x, o, cat, none };
+
+    public class TicTacToeService
+    {
+        public String[,] Board { get; set; }
+
+        public TicTacToeService()
+        {
+            InitializeBoard();
+        }
+
+        public void InitializeBoard()
+        { 
+            Board = new String[3, 3] 
+            { 
+                { "1", "2", "3" }, 
+                { "4", "5", "6"  }, 
+                { "7", "8", "9"  } 
+            };
+        }
+
+        public string GetBoardString()
+        {
+            var sb = new StringBuilder();
+
+            for (int x = 0; x < Board.GetLength(0); x++)
+            {
+                for (int y = 0; y < Board.GetLength(1); y++)
+                {
+                    sb.Append(Board[x, y]);
+                    if (y < Board.GetLength(1) - 1)
+                        sb.Append("|");
+                    else
+                        sb.AppendLine();
+                }
+
+                if (x < Board.GetLength(0) - 1) sb.AppendLine("-----");
+            }
+
+            return sb.ToString();
+        }
+
+        internal bool UpdateCell(int cell, BoardWinner playerTurn)
+        {
+            int? x = null;
+            int? y = null;
+
+            switch (cell)
+            {
+                case 1:
+                    x = 0; y = 0;
+                    break;
+                case 2:
+                    x = 0; y = 1;
+                    break;
+                case 3:
+                    x = 0; y = 2;
+                    break;
+                case 4:
+                    x = 1; y = 0;
+                    break;
+                case 5:
+                    x = 1; y = 1;
+                    break;
+                case 6:
+                    x = 1; y = 2;
+                    break;
+                case 7:
+                    x = 2; y = 0;
+                    break;
+                case 8:
+                    x = 2; y = 1;
+                    break;
+                case 9:
+                    x = 2; y = 2;
+                    break;
+                default:
+                    break;
+            }
+
+            if (x != null && y != null)
+            {
+                if (Board[(int)x, (int)y] == "X" || Board[(int)x, (int)y] == "O") return false;
+                Board[(int)x, (int)y] = playerTurn == BoardWinner.x ? "X" : "O";
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool GameIsOver()
+        {
+            return GetGameWinner() != BoardWinner.none;
+        }
+
+        public BoardWinner GetGameWinner()
+        {
+            // check h 
+            for (int x = 0; x < 3; x++)
+            {
+                if (Board[x, 0] == Board[x, 1] && Board[x, 1] == Board[x, 2])
+                {
+                    return Board[x, 1] == "X" ? BoardWinner.x : BoardWinner.o;
+                }
+            }
+
+            // check v
+            for (int y = 0; y < 3; y++)
+            {
+                if (Board[0, y] == Board[1, y] && Board[1, y] == Board[2, y])
+                {
+                    return Board[0, y] == "X" ? BoardWinner.x : BoardWinner.o;
+                }
+            }
+
+            // check d 
+            if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+            {
+                return Board[0, 0] == "X" ? BoardWinner.x : BoardWinner.o;
+            }
+            if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+            {
+                return Board[0, 2] == "X" ? BoardWinner.x : BoardWinner.o;
+            }
+
+            // check cat 
+            for (int x = 0; x < Board.GetLength(0); x++)
+            {
+                for (int y = 0; y < Board.GetLength(1); y++)
+                {
+                    if (Board[x, y] != "X" && Board[x, y] != "O") return BoardWinner.none;
+                }
+            }
+
+            return BoardWinner.cat;
+        }
+    }
+}
